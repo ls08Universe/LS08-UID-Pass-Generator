295b27aacafd64276bca925ba5d4dfb6b05916e2
ls08Universe <ls08universe@gmail.com>
2024-05-11 22:16:20 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+import string
+
+# Define your ASCII art here
+ascii_art = """
+
+┓ ┏┓┏┓┏┓  ┳┳                ┏┓  ┏┓            ┓  ┏┓               
+┃ ┗┓┃┫┣┫  ┃┃┏┏┓┏┓┏┓┏┓┏┳┓┏┓  ┣╋  ┃┃┏┓┏┏┓┏┏┏┓┏┓┏┫  ┃┓┏┓┏┓┏┓┏┓┏┓╋┏┓┏┓
+┗┛┗┛┗┛┗┛  ┗┛┛┗ ┛ ┛┗┗┻┛┗┗┗   ┗┻  ┣┛┗┻┛┛┗┻┛┗┛┛ ┗┻  ┗┛┗ ┛┗┗ ┛ ┗┻┗┗┛┛ 
+                                                                  
+
+"""
+
+def generate_username():
+    username = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+    return username
+
+def generate_password():
+    password_characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choices(password_characters, k=12))
+    return password
+
+def main():
+    print(ascii_art)  # Print ASCII art
+    while True:
+        username = generate_username()
+        password = generate_password()
+        print("\nGenerated Username:", username)
+        print("Generated Password:", password)
+        choice = input("\nDo you want to generate another username and password? (y/n): ").lower()
+        if choice != 'y':
+            break
+    input("Press Enter to exit...")
+
+if __name__ == "__main__":
+    main()
